940f020814284a89b11553a809eaf40d6400b111
Andrew Lee <andrewimg@gmail.com>
2020-03-20 14:13:51 -0700

    set up basic js/jquery model to get gifs coming in but it is not working. Going to have to do some debugging.

--- logic.js
+++ logic.js
@@ -1,6 +1,44 @@
 var topics = [
     "animals", "basketball", "snowboarding", "guitars", "coding"
 ];
+
+
 for (i=0; i<topics.length; i++) {
     $("#button-holder").append("<button>" + topics[i] + "</button>")
 }
+    
+
+$("button").on("click", function() {
+
+    var search = $(this).attr(topics[i]);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        search + "&api_key=OMvF7MreTMaLsz2K4XCzw2iylRg2DV4i";
+
+$.ajax({
+url: queryURL,
+method: "GET"})
+            
+.then(function(response) {
+            
+var gifs = response.data
+
+for (var h = 0; h < gifs.length; h++) {
+
+
+var gifDiv = $("<div>");
+var rating = results[h].rating;
+var r = $("<p>").text("Rating: " + rating);
+var images = $("<img>");
+
+images.attr("src", results[i].images.fixed_height.url);
+
+
+gifDiv.append(r);
+gifDiv.append(images);
+
+
+$("#gifs-appear-here").prepend(gifDiv);};
+})
+
+});
