3e91dd9f92d4f51e660e9231feebfcd695a2ef96
Andrew Lee <andrewimg@gmail.com>
2020-03-20 13:43:46 -0700

    created a logic.js page and sourced it to the html page. Created a variable that holds an array of topics. Created a for loop to run through the array; next is appending topics to html page as buttons.

---
+++ logic.js
@@ -0,0 +1,7 @@
+var topics = [
+    "animals", "basketball", "snowboarding", "guitars", "coding"
+];
+var button = $('<button></button>')
+for (i=0; i<topics.length; i++) {
+    $("#button-holder").append(button)
+}
